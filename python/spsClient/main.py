d250eb564c8d0871238a4243c440c105d7ae7971
LE FUR Arnaud <arnaud.lefur@lam.fr>
2017-11-21 16:31:55 +0100

    mhs host and port are now defined in /etc

--- python/spsClient/main.py
+++ python/spsClient/main.py
@@ -12,7 +12,7 @@ from mainwindow import SpsWidget
 
 
 class SpsClient(QMainWindow):
-    def __init__(self, reactor, actor, d_width, d_height, addr, port, cmdrName, systemPath):
+    def __init__(self, reactor, actor, d_width, d_height, cmdrName, systemPath):
         QMainWindow.__init__(self)
         self.reactor = reactor
         self.actor = actor
@@ -39,8 +39,6 @@ def main():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--host', default='localhost', type=str, nargs='?', help='tron host ip address')
-    parser.add_argument('--port', default='6093', type=int, nargs='?', help='tron cmdr port')
     parser.add_argument('--name', default=pwd.getpwuid(os.getuid()).pw_name, type=str, nargs='?', help='cmdr name')
     parser.add_argument('--stretch', default=0.6, type=float, nargs='?', help='window stretching factor')
 
@@ -61,8 +59,6 @@ def main():
                    actor,
                    geometry.width() * args.stretch,
                    geometry.height() * args.stretch,
-                   args.host,
-                   args.port,
                    args.name,
                    systemPath)
 
