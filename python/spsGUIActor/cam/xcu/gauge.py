b0908db9f3153dbcd4e710288520cb64eb441517
Arnaud Le Fur <arnaud.lefur@lam.fr>
2019-07-05 16:19:33 +0200

    add Gauge getRaw and setRaw

--- python/spsGUIActor/cam/xcu/gauge.py
+++ python/spsGUIActor/cam/xcu/gauge.py
@@ -1,6 +1,33 @@
 __author__ = 'alefur'
+from spsGUIActor.common import LineEdit
 from spsGUIActor.control import CommandsGB, ControllerPanel
-from spsGUIActor.widgets import ValueGB, CmdButton
+from spsGUIActor.widgets import ValueGB, CmdButton, CustomedCmd
+
+
+class SetRawCmd(CustomedCmd):
+    def __init__(self, controlPanel):
+        CustomedCmd.__init__(self, controlPanel=controlPanel, buttonLabel='SET RAW')
+
+        self.rawCmd = LineEdit()
+        self.addWidget(self.rawCmd, 0, 1)
+
+    def buildCmd(self):
+        cmdStr = '%s setRaw=%s' % (self.controlPanel.actorName,
+                                   self.rawCmd.text())
+        return cmdStr
+
+
+class GetRawCmd(CustomedCmd):
+    def __init__(self, controlPanel):
+        CustomedCmd.__init__(self, controlPanel=controlPanel, buttonLabel='GET RAW')
+
+        self.rawCmd = LineEdit()
+        self.addWidget(self.rawCmd, 0, 1)
+
+    def buildCmd(self):
+        cmdStr = '%s getRaw=%s' % (self.controlPanel.actorName,
+                                   self.rawCmd.text())
+        return cmdStr
 
 
 class GaugePanel(ControllerPanel):
@@ -21,4 +48,10 @@ class GaugeCommands(CommandsGB):
         self.statusButton = CmdButton(controlPanel=controlPanel, label='STATUS',
                                       cmdStr='%s gauge status' % controlPanel.actorName)
 
+        self.getRawCmd = GetRawCmd(controlPanel)
+        self.setRawCmd = SetRawCmd(controlPanel)
+
         self.grid.addWidget(self.statusButton, 0, 0)
+        self.grid.addLayout(self.getRawCmd, 1, 0)
+        self.grid.addLayout(self.setRawCmd, 2, 0)
+
