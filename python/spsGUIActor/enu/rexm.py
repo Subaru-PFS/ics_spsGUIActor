d4adaf3744222080455bc6c5018b158b0b099a72
Arnaud Le Fur <arnaud.lefur@lam.fr>
2019-12-12 20:02:49 +0100

    add enu rexm emergency stop and flag widgets

--- python/spsGUIActor/enu/rexm.py
+++ python/spsGUIActor/enu/rexm.py
@@ -1,9 +1,10 @@
 __author__ = 'alefur'
 
 from spsGUIActor.common import ComboBox, CheckBox
-from spsGUIActor.control import ControllerPanel, ControllerCmd
-from spsGUIActor.widgets import ValueGB, CustomedCmd, AbortButton, DoubleSpinBoxGB
+from spsGUIActor.control import ControllerPanel
 from spsGUIActor.enu import EnuDeviceCmd
+from spsGUIActor.widgets import ValueGB, CustomedCmd, AbortButton, DoubleSpinBoxGB, CmdButton
+
 
 class InitCmd(CustomedCmd):
     def __init__(self, controlPanel):
@@ -56,13 +57,18 @@ class RexmPanel(ControllerPanel):
         self.substate = ValueGB(self.moduleRow, 'rexmFSM', '', 1, '{:s}')
         self.position = ValueGB(self.moduleRow, 'rexm', 'Position', 0, '{:s}')
 
+        self.usrs = ValueGB(self.moduleRow, 'rexmConfig', 'Step Resolution', 7, '{:d}')
+        self.pulseDivisor = ValueGB(self.moduleRow, 'rexmConfig', 'Pulse Divisor', 10, '{:d}')
+        self.standbyCurrent = ValueGB(self.moduleRow, 'rexmConfig', 'Standby Current', 4, '{:d}')
+        self.standbyDelay = ValueGB(self.moduleRow, 'rexmConfig', 'Standby Delay', 41, '{:d}')
+
         self.switchA = ValueGB(self.moduleRow, 'rexmInfo', 'SwitchA', 0, '{:d}')
         self.switchB = ValueGB(self.moduleRow, 'rexmInfo', 'switchB', 1, '{:d}')
         self.speed = ValueGB(self.moduleRow, 'rexmInfo', 'Speed', 2, '{:d}')
         self.steps = ValueGB(self.moduleRow, 'rexmInfo', 'Steps', 3, '{:d}')
 
-        self.usrs = ValueGB(self.moduleRow, 'rexmConfig', 'Step Resolution', 7, '{:d}')
-        self.pulseDivisor = ValueGB(self.moduleRow, 'rexmConfig', 'Pulse Divisor', 10, '{:d}')
+        self.emergencyStop = ValueGB(self.moduleRow, 'rexmStop', 'Emergency Stop', 0, '{:d}')
+        self.emergencyFlag = ValueGB(self.moduleRow, 'rexmStop', 'Emergency Flag', 1, '{:d}')
 
     def setInLayout(self):
         self.grid.addWidget(self.mode, 0, 0)
@@ -72,11 +78,16 @@ class RexmPanel(ControllerPanel):
 
         self.grid.addWidget(self.usrs, 1, 0)
         self.grid.addWidget(self.pulseDivisor, 1, 1)
+        self.grid.addWidget(self.standbyCurrent, 1, 2)
+        self.grid.addWidget(self.standbyDelay, 1, 3)
 
         self.grid.addWidget(self.switchA, 2, 0)
         self.grid.addWidget(self.switchB, 2, 1)
         self.grid.addWidget(self.speed, 2, 2)
         self.grid.addWidget(self.steps, 2, 3)
+
+        self.grid.addWidget(self.emergencyStop, 3, 0)
+        self.grid.addWidget(self.emergencyFlag, 3, 1)
 
 
 class RexmCommands(EnuDeviceCmd):
@@ -88,7 +99,11 @@ class RexmCommands(EnuDeviceCmd):
         self.goCmd = GoCmd(controlPanel=controlPanel)
         self.moveCmd = MoveCmd(controlPanel=controlPanel)
 
+        self.resetFlag = CmdButton(controlPanel=controlPanel, label='RESET FLAG',
+                                   cmdStr='%s rexm resetFlag' % controlPanel.actorName)
+
         self.grid.addLayout(self.initButton, 1, 0, 1, 2)
         self.grid.addWidget(self.abortButton, 2, 2)
         self.grid.addLayout(self.goCmd, 2, 0, 1, 2)
         self.grid.addLayout(self.moveCmd, 3, 0, 1, 2)
+        self.grid.addWidget(self.resetFlag, 4, 0)
